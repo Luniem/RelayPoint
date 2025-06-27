0b07bb7720de9131b837170d7d2d1a4f92f9e892
Nico KNUENZ <nico.KNUENZ@bachmann.info>
2025-06-27 15:37:07 +0000

    feat: implement wifi-handler

---
+++ components/wifi_handler/wifi_handler.c
@@ -0,0 +1,103 @@
+#include "wifi_handler.h"
+#include "esp_log.h"
+
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+
+#define ESP_WIFI_SSID      CONFIG_WIFI_SSID
+#define ESP_WIFI_PASS      CONFIG_WIFI_PASSWORD
+#define ESP_MAXIMUM_RETRY  CONFIG_WIFI_MAXIMUM_RETRY
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG = "WiFi-Handler";
+
+/* FreeRTOS event group to signal when we are connected */
+static EventGroupHandle_t s_wifi_event_group;
+
+static int currRetryNum = 0;
+
+
+// Wi-Fi stack relies on initialized non-volatile-storage
+static void initNVSFlash() {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGI(TAG,"Disonnected from the AP. Trying to reconnect...");
+
+        // we disconnected - try to reconnect if we did not reach max retry count
+        if (currRetryNum < ESP_MAXIMUM_RETRY) {
+            ESP_LOGI(TAG, "Retry to connect to the AP");
+            esp_wifi_connect();
+            currRetryNum++; // count up
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"Reconnect to the AP failed: %d", currRetryNum);
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Received IP:" IPSTR, IP2STR(&event->ip_info.ip));
+        currRetryNum = 0; // reset max-retry for sudden disconnect
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void startupWiFi() {
+    initNVSFlash();
+
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    // register event-handlers and assign instances
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+    //TODO: add possibility to switch authmode
+    // set wifi-config 
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = ESP_WIFI_SSID,
+            .password = ESP_WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    // wait for event bits connected or failed - will be set by the wifi-event-handler
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+
+    // check which event actually happened by checking corresponding bit
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to AP with SSID: %s",
+                 ESP_WIFI_SSID);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to AP with SSID: %s",
+                 ESP_WIFI_SSID);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+}
--- main/main.c
+++ main/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 #include "mqtt_handler.h"
+#include "wifi_handler.h"
 
 void app_main(void)
 {
-    printf("Hello, World!\n");
+    startupWiFi();
     startupMQTT();
 }
