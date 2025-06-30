a17196f1670fd28f262293e9dadef5a640b09180
Nico Knünz <nico.knuenz@aon.at>
2025-07-01 00:40:38 +0200

    feat: init a button_handler (#6)
    * feat: init a button_handler with isr
    
    * feat: receive first mqtt-cmd for status-led

--- components/mqtt_handler/mqtt_handler.c
+++ components/mqtt_handler/mqtt_handler.c
@@ -1,17 +1,30 @@
 #include "mqtt_handler.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
+#include <string.h>
+
+#include "status_led.h"
 
 static const char *TAG = "MQTT-Handler";
 
 static esp_mqtt_client_handle_t client = NULL;
 
+// helper function to copy data to a null-terminated string
+char *copy_to_cstring(const char *data, int len) {
+    char *buf = malloc(len + 1);
+    if (!buf) return NULL;
+    memcpy(buf, data, len);
+    buf[len] = '\0';
+    return buf;
+}
 
 // TODO: Perhaps we log the connected or disconnected state on the display?
 static void mqttEventHandlerCB(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
     switch (event_id) {
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "Connected to MQTT broker");
+
+            esp_mqtt_client_subscribe(client, MQTT_STATUS_LED_CMD_TOPIC, 1);
             break;
 
         case MQTT_EVENT_DISCONNECTED:
@@ -19,9 +32,24 @@ static void mqttEventHandlerCB(void *handler_args, esp_event_base_t base, int32_
             break;
 
         case MQTT_EVENT_DATA:
-            const char *str = (const char *)event_data;
-            ESP_LOGI(TAG, "Received data on topic %s", str);
-            // TODO: Here we handle incoming mqtt messages
+            // cast void pointer to type
+            esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t) event_data;
+
+            // copy the topic and data to null-terminated strings
+            char *topic = copy_to_cstring(event->topic, event->topic_len);
+            char *data = copy_to_cstring(event->data, event->data_len);
+
+            // check what topic we received
+
+            // status LED command topic
+            if (strcmp(topic, MQTT_STATUS_LED_CMD_TOPIC) == 0) {
+                receiveMQTTStatusLEDCommand(data);
+            }
+
+            // free the allocated memory for topic and data
+            free(topic);
+            free(data);
+
             break;
 
         default:
--- components/status_led/status_led.c
+++ components/status_led/status_led.c
@@ -1,13 +1,30 @@
 #include "status_led.h"
 
+#include <stdbool.h>
 #include "esp_log.h"
 #include "driver/gpio.h"
+#include "mqtt_handler.h"
 
 #define STATUS_LED_GPIO GPIO_NUM_0
 
 static const char *TAG = "Status LED";
 
-static int statusLEDState = 0; // start with LED off
+static bool currentStatusLEDState = false; // default state is off
+
+void writeMQTTStatusLED(int statusLEDState) {
+    mqttPublishText(MQTT_STATUS_LED_TOPIC, statusLEDState ? "ON" : "OFF");
+}
+
+void writeStatusLED(int statusLEDState) {
+    currentStatusLEDState = statusLEDState;
+    gpio_set_level(STATUS_LED_GPIO, statusLEDState);
+    ESP_LOGI(TAG, "Status LED wrote to %s!", statusLEDState ? "ON" : "OFF");
+    writeMQTTStatusLED(statusLEDState);
+}
+
+void toggleStatusLED() {
+    writeStatusLED(!currentStatusLEDState);
+}
 
 void initStatusLED() {
     gpio_config_t statusLEDConf = {
@@ -20,11 +37,16 @@ void initStatusLED() {
     
     gpio_config(&statusLEDConf);
     
-    gpio_set_level(STATUS_LED_GPIO, statusLEDState);
+    writeStatusLED(currentStatusLEDState); // Initialize the LED to the default state
 }
 
-void toggleStatusLED() {
-    statusLEDState = !statusLEDState;
-    gpio_set_level(STATUS_LED_GPIO, statusLEDState);
-    ESP_LOGI(TAG, "Status LED toggled to %s!", statusLEDState ? "ON" : "OFF");
+// receive null terminated string data from MQTT and set the status LED accordingly
+void receiveMQTTStatusLEDCommand(const char *data) {
+    if (strcmp(data, "ON") == 0) {
+        writeStatusLED(true);
+    } else if (strcmp(data, "OFF") == 0) {
+        writeStatusLED(false);
+    } else {
+        ESP_LOGW(TAG, "Received unknown command for Status LED: %s", data);
+    }
 }
